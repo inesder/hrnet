6c98c9f1ff52b0fdde5969c20e21f00076922740
inesder <inesderraz@hotmail.fr>
2024-12-12 18:56:34 +0100

    create table component

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,11 +1,22 @@
 import '../styles/styles.css';
+import Table from "../components/Table";
+
 
 function EmployeeList(){
+    const theadData = ["First Name", "Last Name", "Start Date", 'Department', "Date of Birth", "Street", "City", "State", "Zip Code"];
+
+    const tbodyData = [
+        {
+            id: "1",
+            items: ["John", "Clark", "01/01/2025", "Marketing", "01/01/1968", "Park Avenue", "New York", "United States", "10012"],
+        }
+    ];
 return(
         <div id="employee-div" class="container">
             <h1>Current Employees</h1>
             <table id="employee-table" class="display"></table>
             <a href="index.html">Home</a>
+            <Table theadData={theadData} tbodyData={tbodyData} />
         </div>
 )
 }
