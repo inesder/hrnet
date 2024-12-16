1edb0878b5578d1e8104a98b33c5b0e10bcf6108
inesder <inesderraz@hotmail.fr>
2024-12-16 15:22:00 +0100

    add redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
 import employeeReducer from '../features/employeeSlice'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/es/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage, 
+};
+
+const persistedReducer = persistReducer(persistConfig, employeeReducer);
 
 const store = configureStore({
-    reducer: {
-        employees: employeeReducer
-    }
-})
+  reducer: {
+    employees: persistedReducer, 
+  },
+});
 
-export default store
+export const persistor = persistStore(store);
+export default store;
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './app/store';
 import CreateEmployee from './pages/CreateEmployee';
 import EmployeeList from './pages/EmployeeList'
 
@@ -10,12 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <Router>
             <Routes>
                 <Route path="/" element={<CreateEmployee />} />
                 <Route path="/employeelist" element={<EmployeeList/>} />
             </Routes>
         </Router>
+        </PersistGate>
         </Provider>
     </React.StrictMode>
 )
