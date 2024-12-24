b4195a9a4860c888339a7fa92d4566a9543f5b5a
inesder <inesderraz@hotmail.fr>
2024-12-24 18:47:43 +0100

    add filter search

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import TableRow from "./TableRow";
 import TableHeadItem from "./TableHeadItem";
 
 const Table = ({ theadData, tbodyData, customClass }) => {
+    const [filterText, setFilterText] = useState("");
+
+    const handleFilterChange = (e) => {
+        setFilterText(e.target.value);
+    };
+
+    const filteredData = tbodyData.filter((row) => {
+        return row.items.some((item) =>
+            item.toString().toLowerCase().includes(filterText)
+        );
+    });
+
     return (
-        <table className={customClass}>
-            <thead>
-                <tr>
-                    {theadData.map((h) => (
-                        <TableHeadItem key={h} item={h} />
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {tbodyData.map((item) => (
-                    <TableRow key={item.id} data={item.items} />
-                ))}
-            </tbody>
-        </table>
+        <div className="table-container">
+            <input
+                type="text"
+                id="myInput"
+                placeholder="Search..."
+                onChange={handleFilterChange}
+                value={filterText}
+            />
+            <table className={customClass} id="myTable">
+                <thead>
+                    <tr>
+                        {theadData.map((header) => (
+                            <TableHeadItem key={header} item={header} />
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredData.length > 0 ? (
+                        filteredData.map((item) => (
+                            <TableRow key={item.id} data={item.items} />
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={theadData.length} style={{ textAlign: 'center' }}>
+                                No matching employees found.
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
